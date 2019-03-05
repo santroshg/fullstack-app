f39f8e764d12b137dfebae30e000349224c69858
brajesh.anokha <brajesh.anokha@sapient.com>
2019-03-05 10:46:15 +0530

    heroku build/deployment - 1

--- process-management/backend/config/config.js
+++ process-management/backend/config/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/mongo-db',
-  frontendHost: 'http://localhost:3001',
+  // mongoURI: 'mongodb://localhost:27017/mongo-db',
+  // frontendHost: 'http://localhost:3001',
+
+  mongoURI: 'mongodb+srv://Anokha:NBxyXa0FRCKYT05Q@cluster0-amm2q.mongodb.net/mongo-db',
+  frontendHost: 'https://frontend-xt-fsd.herokuapp.com',
 };
--- process-management/frontend/src/constants/constants.ts
+++ process-management/frontend/src/constants/constants.ts
@@ -1,2 +1,5 @@
-export const frontendHost = 'http://localhost:3001';
-export const backtendHost = 'http://localhost:3000';
+// export const frontendHost = 'http://localhost:3001';
+// export const backtendHost = 'http://localhost:3000';
+
+export const frontendHost = 'https://frontend-xt-fsd.herokuapp.com';
+export const backtendHost = 'https://backend-xt-fsd.herokuapp.com';
