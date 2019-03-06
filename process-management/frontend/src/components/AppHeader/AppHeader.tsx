95ca35c6727ccd2f8c653e68903fa03f961618f5
Santosh G <santroshg@gmail.com>
2019-03-06 12:42:02 +0530

    css changes app header and login screen

--- process-management/frontend/src/components/AppHeader/AppHeader.tsx
+++ process-management/frontend/src/components/AppHeader/AppHeader.tsx
@@ -23,20 +23,20 @@ export default class AppHeader extends React.PureComponent<AppHeaderProps, AppHe
       <div className='app-header'>
       <AppBar position="static">
         <Toolbar className="app-header__element">
-        <div>
+        <div className="app-header-text"> 
           <Typography variant="h6" color="inherit" className='app-header__title'>
            <a href="/home" className='app-header__title--link'>Process Management</a> 
           </Typography>
         </div>
-        <div className="userSpace">
+        <div className="app-header-user">
           <Avatar alt="Avatar Img" 
             src={this.props.authenticatedUser.profileImgUrl as string}
-            className="avatar" />
-          <Typography variant="h6" color="inherit" className='app-header__title'>
+            className="app-header-user__avatar" />
+          <Typography variant="h6" color="inherit" className='app-header-user__title'>
             {this.props.authenticatedUser.userDisplayName}
           </Typography>
         </div>
-        <div>
+        <div  className="app-header-logout">
           <Typography variant="h6" color="inherit" className='app-header__title'>
             <a href={`${backtendHost}/users/api/logout`} className='app-header__title--link'>Logout</a> 
           </Typography>
--- process-management/frontend/src/components/AuthComponent/GoogleLoginComponent.tsx
+++ process-management/frontend/src/components/AuthComponent/GoogleLoginComponent.tsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { backtendHost } from '../../constants/constants';
+import { Toolbar, AppBar, Typography } from '@material-ui/core';
 
-interface GoogleLoginComponentProps {}
+interface GoogleLoginComponentProps { }
 
 export default class GoogleLoginComponent extends Component<GoogleLoginComponentProps> {
     constructor(props: GoogleLoginComponentProps) {
@@ -12,17 +13,33 @@ export default class GoogleLoginComponent extends Component<GoogleLoginComponent
 
     render() {
         return (
-            <div className="login-page">
-                <div id="page-wrap">
-                    <div className="google-login-button">
-                        <a href={`${backtendHost}/users/auth/google`} className="button">
-                            <button className="loginBtn loginBtn--google">
-                                Login with Google
+            <Fragment>
+
+                <div className='app-header'>
+                    <AppBar position="static">
+                        <Toolbar className="app-header__element">
+                            <div>
+                                <Typography variant="h6" color="inherit" className='app-header__title'>
+                                    <a href="/home" className='app-header__title--link'>Process Management</a>
+                                </Typography>
+                            </div>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+                <div className="login-component">
+                    <div className="login-wrapper">
+                        <div className='login-text'><h3>Welcome to Process Management</h3><h4>Please Singin</h4></div>
+                        <div className="google-login-button">
+                            <a href={`${backtendHost}/users/auth/google`} className="button">
+                                <button className="loginBtn loginBtn--google">
+                                    Login with Google
                             </button>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fragment>
+
         )
     }
 }
