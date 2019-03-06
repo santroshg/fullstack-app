120cf828f47c1ff4712215ad882d9b0c06795355
brajesh.anokha <brajesh.anokha@sapient.com>
2019-03-06 12:18:37 +0530

    docker wip

--- process-management/backend/config/config.js
+++ process-management/backend/config/config.js
@@ -2,6 +2,9 @@ module.exports = {
   // mongoURI: 'mongodb://localhost:27017/mongo-db',
   // frontendHost: 'http://localhost:3001',
 
+  // frontendHost: 'http://localhost:5000', // for docker
+  // mongoURI: 'mongodb://dockermongodb/mongo-db', // for docker
+
   mongoURI: 'mongodb+srv://Anokha:NBxyXa0FRCKYT05Q@cluster0-amm2q.mongodb.net/mongo-db',
   frontendHost: 'https://frontend-xt-fsd.herokuapp.com',
 };
--- process-management/frontend/src/constants/constants.ts
+++ process-management/frontend/src/constants/constants.ts
@@ -1,5 +1,8 @@
 // export const frontendHost = 'http://localhost:3001';
 // export const backtendHost = 'http://localhost:3000';
 
+// export const frontendHost = 'http://localhost:5000'; // for docker
+// export const backtendHost = 'http://localhost'; // for docker
+
 export const frontendHost = 'https://frontend-xt-fsd.herokuapp.com';
 export const backtendHost = 'https://backend-xt-fsd.herokuapp.com';
